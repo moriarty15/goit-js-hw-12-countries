2d57c0f3c4040bc170459e4665ca261ec0150c23
Vladimir Palamov <84524771+moriarty15@users.noreply.github.com>
2021-09-06 19:07:14 +0800

    проверка на гх

---
+++ src/fetchCountries.js
@@ -0,0 +1,49 @@
+import countryEl from "./templates/country-items.hbs";
+import countriesEl from "./templates/countries.hbs";
+import {notice} from '@pnotify/core';
+
+const divEl = document.querySelector('.country');
+
+function fetchCountries(searchQuery) {
+    fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
+  .then(r => r.json())
+  .then(country => {
+    if (country.length > 10) {
+     const myNotice = notice({
+  text: "необходимо сделать запрос более специфичным."
+});
+    }
+    else if (country.length === 1) {
+        const onceCountry = country[0];
+        divEl.innerHTML = countryEl({onceCountry});
+    }
+    else if (country.length <= 10) {
+      console.log(country);
+        divEl.innerHTML = countriesEl({country})
+    }
+    
+  })
+}
+export default fetchCountries;
+
+
+
+
+// const myAlert = alert({
+//   text: "I'm an alert.",
+//   type: 'info'
+// });
+
+
+
+// const myInfo = info({
+//   text: "I'm an info message."
+// });
+
+// const mySuccess = success({
+//   text: "I'm a success message."
+// });
+
+// const myError = error({
+//   text: "I'm an error message."
+// });
--- src/index.js
+++ src/index.js
@@ -1,48 +1,11 @@
-import samplesHTML from './templates/gallery-items.hbs';
-import menu from './menu.json';
+import { debounce } from "lodash";
+import fetchCountries from "./fetchCountries";
 
-const menuEl = document.querySelector('.js-menu');
+const inputEl = document.querySelector('.input');
 
-menuEl.insertAdjacentHTML('beforeend', createMenu(menu));
+inputEl.addEventListener('input', debounce(inputCatch, 500))
 
-function createMenu(menu) {
-  return samplesHTML({menu})
-}
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
-
-const checkboxEl = document.querySelector('#theme-switch-toggle');
-
-checkboxEl.addEventListener('change', getThemeToBody)
-function getThemeToBody() {
-  if (document.body.classList.contains('dark-theme')) {
-    document.body.classList.remove('dark-theme');
-    document.body.classList.add('light-theme');
-    localStorage.setItem('currentTheme', Theme.LIGHT)
-  }
-  else if (document.body.classList.contains('light-theme')) {
-    document.body.classList.remove('light-theme');
-    document.body.classList.add('dark-theme');
-    localStorage.setItem('currentTheme', Theme.DARK)
-  }
-  else {
-    document.body.classList.add('dark-theme');
-    localStorage.setItem('currentTheme', Theme.DARK);
-  }
-}
-
-if (localStorage.getItem('currentTheme') === 'light-theme') {
-  document.body.classList.remove('dark-theme');
-  document.body.classList.add('light-theme');
-  localStorage.setItem('currentTheme', Theme.LIGHT);
-} else if (localStorage.getItem('currentTheme') === 'dark-theme') {
-  document.body.classList.remove('light-theme');
-  document.body.classList.add('dark-theme');
-  localStorage.setItem('currentTheme', Theme.DARK);
-  checkboxEl.checked = 'true';
-} else if (localStorage.getItem('currentTheme') === null) {
-  document.body.classList.add('light-theme');
-  localStorage.setItem('currentTheme', Theme.LIGHT);
+function inputCatch(e) {
+  console.log(e.target.value);
+  return fetchCountries(e.target.value);
 }
