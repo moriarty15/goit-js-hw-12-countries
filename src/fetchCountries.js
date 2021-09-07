281e9e5505375a620cdbb075458d6620876ad4fd
Vladimir Palamov <84524771+moriarty15@users.noreply.github.com>
2021-09-07 17:28:18 +0800

    рефакторинг кода, исправление замечаний ментора

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,27 +1,15 @@
-import countryEl from "./templates/country-items.hbs";
-import countriesEl from "./templates/countries.hbs";
-import {notice} from '@pnotify/core';
-
-const divEl = document.querySelector('.country');
+import { error } from '@pnotify/core';
 
 function fetchCountries(searchQuery) {
-    fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
-  .then(r => r.json())
-  .then(country => {
-    if (country.length > 10) {
-     const myNotice = notice({
-  text: "необходимо сделать запрос более специфичным."
-});
-    }
-    else if (country.length === 1) {
-        const onceCountry = country[0];
-        divEl.innerHTML = countryEl({onceCountry});
-    }
-    else if (country.length <= 10) {
-      console.log(country);
-        divEl.innerHTML = countriesEl({country})
-    }
-    
-  })
-}
+  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
+    .then(responce => {
+      if (!responce.ok) return error({
+  text: "I'm an error message."})
+      return responce.json();
+    })
+    .catch(e => error({
+  text: "I'm an error message."}))
+  }
 export default fetchCountries;
+
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import './sass/main.scss';
 import { debounce } from "lodash";
 import fetchCountries from "./fetchCountries";
+import countryEl from "./templates/country-items.hbs";
+import countriesEl from "./templates/countries.hbs";
+import {notice} from '@pnotify/core';
+
 
 const inputEl = document.querySelector('.input');
+const divEl = document.querySelector('.country');
 
 inputEl.addEventListener('input', debounce(inputCatch, 500))
 
 function inputCatch(e) {
   console.log(e.target.value);
-  return fetchCountries(e.target.value);
+  if (e.target.value == ' ' || e.target.value == '') return
+  const fetchResolt = fetchCountries(e.target.value);
+  renderHTML(fetchResolt);
 }
+
+function renderHTML(fetchResolt) {
+  fetchResolt.then(country => {
+    if (country.length > 10) {
+     notice({
+       text: "необходимо сделать запрос более специфичным."
+       });
+    }
+    else if (country.length === 1) {
+      console.log(country);
+        const onceCountry = country[0];
+        divEl.innerHTML = countryEl({onceCountry});
+    }
+    else if (country.length <= 10) {
+      console.log(country);
+
+        divEl.innerHTML = countriesEl({country})
+    }
+  })
+}
+
+
